36ef8440cb116bc2f7ae383f749b2433b5938ab1
Thomas Wilbur <thomaswilbur@adaclare.com>
2022-07-10 01:27:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,10 +21,12 @@ curl https://openlitespeed.org/packages/openlitespeed-$OSLSWSVER.tgz -o openlite
 tar xvf openlitespeed.tar.xz
 rm -rf openlitespeed.tar.xz
 cd openlitespeed
+clear
 echo " INSTALLING OPEN LITESPEED "
 ./install.sh
 cd $PWDHOME
 rm -rf openlitespeed
+clear
 echo " INSTALLING COMPATIBLE LSWS PHP VERSIONS "
 echo "LSPHP 7.4"
 yum -y -q remove lsphp74*
@@ -35,12 +37,14 @@ yum -y -q install lsphp73*
 echo "LSPHP 7.2"
 yum -y -q remove lsphp72*
 yum -y -q install lsphp72*
+clear
 echo " STARTING LSWS WEB SERVER "
 systemctl disable httpd
 systemctl stop httpd
 systemctl start lshttpd
 systemctl enable lshttpd
 systemctl status lshttpd
+clear
 echo " SETTING UP CPANEL LITESPEED CONVERSION SCRIPT "
 rm -rf /var/run/chkservd/apache*
 cp -rf conversion /usr/local/lsws/configparse
@@ -51,11 +55,19 @@ systemctl start lswsparse
 systemctl enable lswsparse
 systemctl status lswsparse
 bash updateService.sh
+clear
 echo " Installing Modified Litespeed cPanel Extension "
 cd lsws_whm_plugin && bash lsws_whm_plugin_install.sh
-
+clear
 echo " "
 echo " "
-echo " APACHE CONVERTED TO LSWS DONE! "
+echo " Thank you for using the Adaclare Openlitespeed cPanel Plugin "
+echo " NOTE: It may take some time for LSWSParse to generate a config and for litespeed to come online"
+echo " Please monitor using the following commands: "
+echo " "
+echo " systemctl status lshttpd "
+echo " systemctl status lswsparse "
+echo " "
+echo " If you run into issues, please create a issue here and we will have a look at it. "
 echo " "
 echo " "
