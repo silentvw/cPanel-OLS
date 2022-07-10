b661cf383ae2d846f910e1796c51169bf00b313e
root <root@server.besthost.pro>
2022-07-10 01:02:07 -0500

    Fixed LSWS not starting

--- updateService.sh
+++ updateService.sh
@@ -1,4 +1,9 @@
+#!/bin/bash
+echo "Stopping LSWS Parse & LSHTTPD"
 systemctl stop lswsparse
+systemctl disable lshttpd
+systemctl stop lshttpd
+echo "Updating Files"
 rm -rf /usr/local/lsws/conf/httpd_config.conf
 cp httpd_config.conf /usr/local/lsws/conf/httpd_config.conf
 rm -rf /usr/local/lsws/configparse
@@ -7,5 +12,10 @@ cp -rf lswsparse.service /etc/systemd/system/lswsparse.service
 rm -rf /usr/local/lsws/conf/httpd_config.conf
 cp -rf httpd_config.conf /usr/local/lsws/conf/
 rm -rf /usr/local/lsws/.changesDetect
+systemctl enable lshttpd
 systemctl daemon-reload
 systemctl start lswsparse
+echo "Waiting for LSWSparse to generate our config"
+sleep 3s
+echo "Starting Litespeed..."
+systemctl start lshttpd
