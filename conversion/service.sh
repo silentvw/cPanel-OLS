b69a1a04133f3effae8acb8863d4539734d6fee5
Thomas Wilbur <thomaswilbur@adaclare.com>
2022-07-10 21:51:55 -0500

    Update service.sh

--- conversion/service.sh
+++ conversion/service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while true; do
-sysctl -p fs.enforce_symlinksifowner=1
+sysctl fs.enforce_symlinksifowner=1
 systemctl stop httpd
 systemctl disable httpd
 cd /usr/local/lsws/configparse/
